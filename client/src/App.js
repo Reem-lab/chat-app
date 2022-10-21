d39adb294b07b7a785dcb82259fa58d3c38d9c72
reem <janinareem@gmail.com>
2022-10-21 23:40:23 +0200

    Add App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,20 @@ const socket = io.connect("http://localhost:3001");
 const App = () => {
   const [username, setUsername ] = useState("");
   const [room, setRoom ] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
    const joinRoom = () => {
       if(username !== "" && room !== ""){
-        socket.emit("join_room", room)
+        socket.emit("join_room", room);
+
+        setShowChat(true);
       }
    }
 
   return (
     <div className="App"> 
+    { !showChat ? (
+    <div className='joinChatContainer'>
       <h3>Join Chat Room</h3>
       <input 
       type="text" 
@@ -28,7 +33,10 @@ const App = () => {
        onChange={(e) => setRoom(e.target.value) }/>
       <button onClick={joinRoom}>Join Room</button>
 
+      </div>
+    ) : (
       <Chat socket={socket} username={username} room={room} />
+    )}
     </div>
   );
 }
